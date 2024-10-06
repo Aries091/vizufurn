6e2c3b3883263733a9a4c310c8e6fefaceb8593e
Aries091 <97426155+Aries091@users.noreply.github.com>
2024-10-06 10:25:32 +0545

    feat: updating api

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -68,4 +68,91 @@ const publishProduct = asyncHandler(async (req, res) => {
         return res.status(500).json(new ApiResponse(500, null, "Internal Server Error"));
     }
 });
-export { publishProduct };
+
+
+const deleteProduct =asyncHandler(async(req,res)=>{
+
+    try{
+        const productId = req.params.productId;
+       
+        if(!productId){
+            throw new ApiError(400, "Product ID is required");
+        }
+        const product = await Product.findById(productId)
+
+        if(!product){
+            throw new ApiError(404, "Product not found");
+        }
+        
+        await Product.findByIdAndDelete(productId);
+        return res.status(200)
+        .json(new ApiResponse(200, null, "Product deleted successfully"));
+
+        
+    }
+    catch(error){
+        console.error("Error in deleteProduct",error);
+        if(error instanceof ApiError){
+            return res.status(error.statusCode)
+            .json(new ApiResponse(error.statusCode, null, error.message));
+
+    }
+}
+})
+const updateProduct = asyncHandler(async(req,res)=>{
+    try{
+        const{productId}=req.params;
+        const {name,description,price,stock}=req.body;
+        if(!productId){
+            throw new ApiError(400, "Product ID is required");
+        }
+        const product = await Product.findById(productId);
+        if(!product){
+            throw new ApiError(404, "Product not found");
+        }
+        if(name) product.name=name;
+        if(description)product.description=description;
+        if(price) product.price =price;
+        if(stock) product.stock=stock;
+
+        if(category){
+            if(!Object.values(CATEGORIES).includes(category)){
+                throw new ApiError(400, "Invalid category");
+            }
+            product.category=category;
+
+        }
+        const displayImageLocalPath=req.files?.dislayImage?.pth;
+        if(displayImageLocalPath){
+            const displayImage = await uploadOnCloudinary(displayImageLocalPath);
+            if(displayImage){
+                product.displayImage=displayImage.url;
+            }
+        }
+        const modelImageLocalPath = req.files?.displayImage?.[0]?.path;
+        if(modelImageLocalPath){
+            const modelImage =await uploadOnCloudinary(modelImageLocalPath);
+            if(modelImage){
+                product.modelImage=modelImage.url;
+            }
+        }
+        await product.save();
+        return res.status(200).json (
+            new ApiResponse(200, product, "Product updated successfully")
+        );
+
+
+    }
+    catch(error){
+        console.error("Error in updateProduct",error)
+        if (error instanceof ApiError){
+            return res.status(error.status)
+            .json( new ApiResponse(error.statusCode,null,error.message));
+            
+        }
+        return res.status(500).json(new ApiResponse(500,null,"Internal Server Error"));
+    }
+})
+
+
+export { publishProduct,deleteProduct };
